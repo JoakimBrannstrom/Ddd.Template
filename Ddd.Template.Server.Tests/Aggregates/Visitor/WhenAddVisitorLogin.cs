2521e50fc87f2611f997eb3ac176393125374715
Joakim Brännström <tok_jocke@hotmail.com>
2013-06-23 00:44:40 +0200

    Added some visitor domain tests

---
+++ Ddd.Template.Server.Tests/Aggregates/Visitor/WhenAddVisitorLogin.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Ddd.Template.Contracts.Commands.Visitor;
+using Ddd.Template.Contracts.Events;
+using Ddd.Template.Contracts.Events.Visitor;
+using Ddd.Template.Domain.CommandHandlers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Ddd.Template.Server.Tests.Aggregates.Visitor
+{
+	public abstract class WhenAddVisitorLogin : CommandTestFixture<Domain.Aggregates.Visitor, AddVisitorLogin, VisitorCommandHandler>
+	{
+		protected abstract Guid UserId { get; }
+
+		protected override IEnumerable<Event> Given()
+		{
+			return new[] { new VisitorArrived { AggregateId = AggregateId } };
+		}
+
+		protected override AddVisitorLogin When()
+		{
+			return new AddVisitorLogin
+			{
+				AggregateId = AggregateId,
+				CommandId = Guid.NewGuid(),
+				OriginalVersion = 1,
+
+				UserId = UserId
+			};
+		}
+
+		[TestClass]
+		public class WhenAddVisitorLogin_WithProperUserId : WhenAddVisitorLogin
+		{
+			protected override Guid UserId { get { return Guid.NewGuid(); } }
+
+			[TestMethod]
+			public void OneEventShouldBePublishedOnTheBus()
+			{
+				VerifyThatEventsWasPublishedOnTheBus(Times.Once());
+			}
+
+			[TestMethod]
+			public void ThePublishedEventShouldBeLinkWasVisited()
+			{
+				VerifyThatEventPublishedOnTheBusIsOfCorrectType<VisitorLoggedIn>();
+			}
+
+			[TestMethod]
+			public void ThePublishedEventShouldHaveCurrentTimestamp()
+			{
+				VerifyThatEventsPublishedOnTheBusHaveCurrentTimestamp();
+			}
+		}
+	}
+}
---
+++ Ddd.Template.Server.Tests/Aggregates/Visitor/WhenVisitorIsAdded.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using Ddd.Template.Contracts;
+using Ddd.Template.Contracts.Commands.Visitor;
+using Ddd.Template.Contracts.Events;
+using Ddd.Template.Contracts.Events.Visitor;
+using Ddd.Template.Domain.CommandHandlers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Ddd.Template.Server.Tests.Aggregates.Visitor
+{
+	public class WhenVisitorIsAdded : CommandTestFixture<Domain.Aggregates.Visitor, AddVisitor, VisitorCommandHandler>
+	{
+		protected override AddVisitor When()
+		{
+			return new AddVisitor
+			{
+				AggregateId = AggregateId,
+				CommandId = Guid.NewGuid(),
+				Created = DateTime.Now,
+				Platform = "Platform",
+				UserAgent = "UserAgent",
+				UserHostAddress = "UserHostAddress",
+				UserHostName = "UserHostName",
+				UserLanguages = new List<string> { "UserLanguages" },
+				OriginalVersion = 0
+			};
+		}
+
+		[TestClass]
+		public class WhenVisitorArrived_WithNoHistory : WhenVisitorIsAdded
+		{
+			[TestMethod]
+			public void OneEventShouldBePublishedOnTheBus()
+			{
+				VerifyThatEventsWasPublishedOnTheBus(Times.Once());
+			}
+
+			[TestMethod]
+			public void ThePublishedEventShouldBeVisitorArrived()
+			{
+				VerifyThatEventPublishedOnTheBusIsOfCorrectType<VisitorArrived>();
+			}
+
+			[TestMethod]
+			public void ThePublishedEventShouldHaveCurrentTimestamp()
+			{
+				VerifyThatEventsPublishedOnTheBusHaveCurrentTimestamp();
+			}
+		}
+
+		[TestClass]
+		public class WhenVisitorArrived_CalledTwice : WhenVisitorIsAdded
+		{
+			protected override Type GetExpectedExceptionType()
+			{
+				return typeof(ConcurrencyException);
+			}
+
+			protected override IEnumerable<Event> Given()
+			{
+				return new[] { new VisitorArrived { AggregateId = AggregateId } };
+			}
+
+			[TestMethod]
+			public void ConcurrencyExceptionShouldBeThrown()
+			{
+				VerifyLastException<ConcurrencyException>();
+			}
+			[TestMethod]
+			public void NoEventsShouldBePublishedOnTheBus()
+			{
+				VerifyThatNoEventsWasPublishedOnTheBus();
+			}
+		}
+	}
+}
